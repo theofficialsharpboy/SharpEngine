d949e4ecdb19470d6a828ce62b1c493d5a553a8d
Stephen Hale <contact@thesharpboy.com>
2025-03-03 23:24:08 +0000

    Added notification/popups component.

---
+++ SharpEngine/Comnponents/NotificationDirection.cs
@@ -0,0 +1,24 @@
+namespace SharpEngine.Comnponents;
+
+public enum NotificationDirection
+{
+    /// <summary>
+    /// The notification will slide in from the left.
+    /// </summary>
+    Left    = 0x1F,
+
+    /// <summary>
+    /// The notification will slide in from the right.
+    /// </summary>
+    Right   = 0x2F,
+
+    /// <summary>
+    /// The notification will slide in from the top.
+    /// </summary>
+    Top     = 0x3F,
+
+    /// <summary>
+    /// The notification will slide in from the bottom.
+    /// </summary>
+    Down    = 0x4F,
+}
